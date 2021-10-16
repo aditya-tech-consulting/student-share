4a042076d78fbce2f0cefe02d5562a95c1fbe6de
aditya-pratap-bhuyan <aditya.bhuyan@yahoo.com>
2021-10-16 20:15:53 +0530

    Updating Session 6 for Feign Client

--- session6-springcloud/code/spring-cloud-demo/booking-service/src/main/java/org/example/spring/cloud/booking/service/BookingClient.java
+++ session6-springcloud/code/spring-cloud-demo/booking-service/src/main/java/org/example/spring/cloud/booking/service/BookingClient.java
@@ -1,9 +1,10 @@
 package org.example.spring.cloud.booking.service;
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+//import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("seating-server")
+@FeignClient(value = "seating-server")
 public interface BookingClient {
     @RequestMapping("/seats")
     String available();
--- session6-springcloud/code/spring-cloud-demo/booking-service/src/main/java/org/example/spring/cloud/booking/service/BookingServiceApplication.java
+++ session6-springcloud/code/spring-cloud-demo/booking-service/src/main/java/org/example/spring/cloud/booking/service/BookingServiceApplication.java
@@ -2,11 +2,12 @@ package org.example.spring.cloud.booking.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+//import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients //Ribbon
+@EnableFeignClients(clients = BookingClient.class)
 public class BookingServiceApplication {
 
     public static void main(String[] args) throws Exception {
